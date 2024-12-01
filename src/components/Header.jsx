12af30cc51d3fe82e5e02a7edfac9a930b1cf299
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-12-01 12:26:42 -0500

    Enhance Debugger and Header components; update button styles and layout; modify color scheme in CSS files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,11 @@ import '../styles/Header.css';
 const Header = () => {
     return (
         <header className='HeaderSection'>
-            <h1 className='titleHeader'>MIPS Visualiser</h1>
+            <h1 className='titleHeader'>MIPS Visual Simulator</h1>
         </header>
     );
 };
 
 export default Header;
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 6v12l10-6z"></path></svg>
