560de4e5602db929a5b995b83a39038bfa0cdf63
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-12-01 13:32:38 -0500

    Update styles in Debugger, Header, and MIPS components; change button SVG colors, adjust font family to Montserrat, and refine border-radius and height properties

--- src/components/Debugger.jsx
+++ src/components/Debugger.jsx
@@ -5,8 +5,15 @@ const ControlButtons = ({ simulateMIPS, stepMIPS, stepBackMIPS, resetMIPS }) => 
   return (
     <div id="control-buttons" className='control-buttons'>
             <button onClick={resetMIPS}>
-      <svg fill="#f8f8f8" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" stroke="#f8f8f8"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M64,256H34A222,222,0,0,1,430,118.15V85h30V190H355V160h67.27A192.21,192.21,0,0,0,256,64C150.13,64,64,150.13,64,256Zm384,0c0,105.87-86.13,192-192,192A192.21,192.21,0,0,1,89.73,352H157V322H52V427H82V393.85A222,222,0,0,0,478,256Z"></path></g></svg>
+            <svg fill="#ffffff" viewBox="0 0 512 512" data-name="Layer 1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M64,256H34A222,222,0,0,1,430,118.15V85h30V190H355V160h67.27A192.21,192.21,0,0,0,256,64C150.13,64,64,150.13,64,256Zm384,0c0,105.87-86.13,192-192,192A192.21,192.21,0,0,1,89.73,352H157V322H52V427H82V393.85A222,222,0,0,0,478,256Z"></path>
+              </g>
+            </svg>
       </button>
+      
       <button onClick={simulateMIPS}>
         <svg viewBox="0 0 24 24" width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill="#f8f8f8" d="M8 5v14l11-7z"/>
